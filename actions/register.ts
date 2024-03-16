e6f3a476ea7e247094ff1f02ec154498baa32517
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2024-03-16 18:41:25 +0700

    feat: add bcrypt to encrypt password & validate email existed

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,7 +13,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         };
     }
 
+    const { email, password, name } = validatedFields.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return {
+            error: "Email already in use!",
+        };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    // TODO: Send verification token email
+
     return {
-        success: "Register success!",
+        success: "User created!",
     };
 };
